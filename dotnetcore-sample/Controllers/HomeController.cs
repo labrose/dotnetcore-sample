0d85e28c73a9132974914f82d4b702fd1e1eb8d3
Alex Nichols <19493383+alexcnichols@users.noreply.github.com>
2018-01-30 19:29:07 -0800

    Add what's new to About page

--- dotnetcore-sample/Controllers/HomeController.cs
+++ dotnetcore-sample/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace dotnetcore_sample.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "What's new in VSTS.";
 
             return View();
         }
